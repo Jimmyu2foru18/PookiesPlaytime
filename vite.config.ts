b08b3d0537dd7a0cc1305a0726b8a734f77c5a47
Jimmy McG <jimmymcguigan18@gmail.com>
2025-12-26 23:06:49 -0500

    Use relative base and rebuild dist for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,17 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
-    return {
-      base: '/PookiesPlaytime/', 
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  return {
+    base: './',
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-    };
+    }
+  };
 });
